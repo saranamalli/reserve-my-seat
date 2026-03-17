0e577826f65f4eea6371e8e1063be5fa467218fb
MallikarjunaGupta S G M R N S <saranamalli694@gmail.com>
2026-03-17 21:26:56 +0530

    Feature: Initialized Gateway Service

---
+++ gateway-service/src/test/java/com/reserve_my_seat/gateway_service/GatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reserve_my_seat.gateway_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
