d975c20db4adb73c87a70833caf07d2a0ddeb905
MallikarjunaGupta S G M R N S <saranamalli694@gmail.com>
2026-03-20 22:43:29 +0530

    Feature: Redis dependency added.

--- homepage-service/src/main/java/com/reserve_my_seat/homepage_service/HomepageServiceApplication.java
+++ homepage-service/src/main/java/com/reserve_my_seat/homepage_service/HomepageServiceApplication.java
@@ -1,13 +1,31 @@
 package com.reserve_my_seat.homepage_service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+import java.util.Map;
 
 @SpringBootApplication
-public class HomepageServiceApplication {
+public class HomepageServiceApplication implements ApplicationRunner {
+
+	@Autowired
+	private StringRedisTemplate stringRedisTemplate;
+
+	private final Logger logger = LoggerFactory.getLogger(HomepageServiceApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(HomepageServiceApplication.class, args);
 	}
 
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		stringRedisTemplate.opsForValue().set("test", "hello");
+		logger.info("Redis communication successful: {}", Map.of("test", stringRedisTemplate.opsForValue().get("test")));
+	}
 }
